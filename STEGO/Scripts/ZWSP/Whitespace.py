aaad528ccb13b58e9e7340ee9a679f4f403e997b
Chaitanya Krishna Chauhan <95117634+ckc1404@users.noreply.github.com>
2022-11-29 16:50:20 +0530

    Create Whitespace.py

---
+++ STEGO/Scripts/ZWSP/Whitespace.py
@@ -0,0 +1,27 @@
+text='                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                '
+
+
+
+
+
+first=' '
+
+second=' '
+
+binary=''
+
+
+
+for char in text:
+
+   if char==first:
+
+      binary+='0'
+
+   else:
+
+      binary+='1'
+
+print(binary)
+
+#Example
