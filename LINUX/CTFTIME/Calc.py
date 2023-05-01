22c15f0b1941c398de49d8f9d8dbfc0ef0214e65
Chaitanya Krishna Chauhan <95117634+ckc9759@users.noreply.github.com>
2023-05-02 00:52:12 +0530

    Update Calc.py

--- LINUX/CTFTIME/Calc.py
+++ LINUX/CTFTIME/Calc.py
@@ -1,10 +1,30 @@
+#f=open('ctf.txt','r')
+#points_list=[]
+
+#for lines in f:
+   #points_list.append(lines[-6:-1])
+    
+#print(points_list)
+
+#---Update
+
 f=open('ctf.txt','r')
+
 points_list=[]
 
 for lines in f:
-   points_list.append(lines[-6:-1])
-    
+   if lines[-7].isnumeric():
+      points_list.append(lines[-7:-1])
+   else:
+      points_list.append(lines[-6:-1])
+   
 print(points_list)
+points_list=[eval(i) for i in points_list]
+points_list.sort(reverse=True)
+print()
+print(points_list)
+   
+
 
    
 
