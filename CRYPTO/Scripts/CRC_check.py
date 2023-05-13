d9857dce48ec525da6defb83853e270a44102927
Chaitanya Krishna Chauhan <95117634+ckc9759@users.noreply.github.com>
2023-05-13 20:45:06 +0530

    Create CRC_check.py

---
+++ CRYPTO/Scripts/CRC_check.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from pwn import p32
+from zlib import crc32
+
+
+required_crc = 0x0699358E
+max_dimension = 4000
+
+for width in range(0, max_dimension):
+    for height in range(0, max_dimension):
+        ihdr = b'\x49\x48\x44\x52' + p32(width, endian='big') + p32(height, endian='big') + b'\x08\x06\x00\x00\x00'
+        
+        if height % 100 == 0:
+            print('ihdr:', ihdr.hex())
+        
+        crc = crc32(ihdr)
+        if crc == required_crc:
+            print('FOUND!')
+            print(width, height)
+            exit()
