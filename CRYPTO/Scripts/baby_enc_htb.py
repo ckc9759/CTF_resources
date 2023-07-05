beef4c7d7ab970de4f67fb3d9ece854cde6c9233
Chaitanya Krishna Chauhan <95117634+ckc9759@users.noreply.github.com>
2023-07-05 17:33:52 +0530

    Update baby_enc_htb.py

--- CRYPTO/Scripts/baby_enc_htb.py
+++ CRYPTO/Scripts/baby_enc_htb.py
@@ -37,3 +37,19 @@ print(bytes(pt))
 # ct = ((123*char+18) % 256
 
 #char=((ct-18)/123*256)%256
+
+#Bruteforce approach !
+'''
+encrypted_txt = "6e0a9372ec49a3f6930ed8723f9df6f6720ed8d89dc4937222ec7214d89d1e0e352ce0aa6ec82bf622227bb70e7fb7352249b7d893c493d8539dec8fb7935d490e7f9d22ec89b7a322ec8fd80e7f8921"
+result = ""
+
+ct = bytes.fromhex(encrypted_txt)
+ 
+for char in ct:
+    for val in range(33, 126):
+        if ((123 * val + 18) % 256) == char:
+            result += chr(val)
+            break
+ 
+print(result)
+'''
