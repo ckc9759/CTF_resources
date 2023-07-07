9e159f44ab755dd23473e6a84cdca762fdf16913
Chaitanya Krishna Chauhan <95117634+ckc9759@users.noreply.github.com>
2023-07-07 19:16:04 +0530

    Update Calc.py

--- LINUX/CTFTIME/Calc.py
+++ LINUX/CTFTIME/Calc.py
@@ -8,6 +8,25 @@
 
 #---Update
 
+'''f=open('ctf.txt','r')
+
+points_list=[]
+
+for lines in f:
+   if lines[-7].isnumeric():
+      points_list.append(lines[-7:-1])
+   else:
+      points_list.append(lines[-6:-1])
+   
+print(points_list)
+points_list=[eval(i) for i in points_list]
+points_list.sort(reverse=True)
+print()
+print(points_list)
+'''
+
+#---Update 2
+
 f=open('ctf.txt','r')
 
 points_list=[]
@@ -23,6 +42,16 @@ points_list=[eval(i) for i in points_list]
 points_list.sort(reverse=True)
 print()
 print(points_list)
+print()
+
+sum=0.0
+for i in range(9):
+   sum+=points_list[i]
+   
+sum+=2*23.91
+print(f'sum={sum}')
+
+
    
 
 
