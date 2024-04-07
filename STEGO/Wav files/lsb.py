efc346b1d068db14ea41bee7c636a819c9a06189
Chaitanya Krishna Chauhan <95117634+ckc9759@users.noreply.github.com>
2024-04-07 12:35:10 +0530

    Update lsb.py

--- STEGO/Wav files/lsb.py
+++ STEGO/Wav files/lsb.py
@@ -13,3 +13,22 @@ string_blocks = (extractedLSB[i:i+8] for i in range(0, len(extractedLSB), 8))
 decoded = ''.join(chr(int(char, 2)) for char in string_blocks)
 print(decoded[0:500])
 wav.close()
+
+---
+
+# Use wave package (native to Python) for reading the received audio file
+import wave
+song = wave.open("song_embedded.wav", mode='rb')
+# Convert audio to byte array
+frame_bytes = bytearray(list(song.readframes(song.getnframes())))
+
+# Extract the LSB of each byte
+extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
+# Convert byte array back to string
+string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
+# Cut off at the filler characters
+decoded = string.split("###")[0]
+
+# Print the extracted text
+print("Sucessfully decoded: "+decoded)
+song.close()
