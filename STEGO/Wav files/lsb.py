b8462dc5d5eb14820905b25934974391bdab1665
Chaitanya Krishna Chauhan <95117634+ckc9759@users.noreply.github.com>
2024-04-07 12:30:03 +0530

    Update lsb.py

--- STEGO/Wav files/lsb.py
+++ STEGO/Wav files/lsb.py
@@ -1,43 +1,15 @@
-#!/usr/bin/python
-
 import wave
-
 import struct
-
-
-
-wav= wave.open("mys.wav", mode='rb')
-
-print (wav.getparams())
-
-
-
+wav = wave.open("audio.wav", mode='rb')
 frame_bytes = bytearray(list(wav.readframes(wav.getnframes())))
-
-print(frame_bytes[:100])
-
-
-
 shorts = struct.unpack('H'*(len(frame_bytes)//2), frame_bytes)
-
-    
-
-# Get all LSB's
-
+extracted_left = shorts[::2] 
+extracted_right = shorts[1::2]
 extractedLSB = ""
-
-for i in range(0, len(shorts)):
-
-        extractedLSB += str(shorts[i] & 1 )
-
-# divide strings into blocks of eight binary strings
-
-# convert them and join them back to string
-
+for i in range(0, len(extracted_left)):
+    extractedLSB += (str(extracted_left[i] & 1)) if i%2==0 else (str(extracted_right[i] & 1))
+    
 string_blocks = (extractedLSB[i:i+8] for i in range(0, len(extractedLSB), 8))
-
 decoded = ''.join(chr(int(char, 2)) for char in string_blocks)
-
-print(decoded[:500])
-
+print(decoded[0:500])
 wav.close()
