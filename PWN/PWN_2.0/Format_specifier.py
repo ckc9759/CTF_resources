0ca8a7628dae44de21a60e819cf12fe250657818
Chaitanya Krishna Chauhan <95117634+ckc9759@users.noreply.github.com>
2025-03-20 19:45:44 +0530

    Update Format_specifier.py

--- PWN/PWN_2.0/Format_specifier.py
+++ PWN/PWN_2.0/Format_specifier.py
@@ -8,6 +8,7 @@ from pwn import *
 binary.context = context = './filename'
 
 payload="%lX.%lX.%lX.%lX.%lX.%lX.%lX.%lX.%lX.%lX."
+#modified payload = "%6$lX.%7$lX.%8$lX.%9$lX.%10$lX."
 
 p = process()
 p.recv()
